de13b8b9fac1ee43269780d207a83405270e1ac7
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-30 22:49:19 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,39 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { ReactComponent as IconClose } from '../icons/x-close.svg';
 import * as s from './Modal.styled';
 import PropTypes from 'prop-types';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal({ closeModal, currentImage, tags }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  componentDidUpdate() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  handleBackdropClick = evt => {
+  const handleBackdropClick = evt => {
     if (evt.currentTarget === evt.target) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    return (
-      <s.Overlay onClick={this.handleBackdropClick}>
-        <s.Modal>
-          <s.CloseButton type="button" onClick={this.props.closeModal}>
-            <IconClose width="30" heigth="30" />
-            <s.ButtonLabel>Close</s.ButtonLabel>
-          </s.CloseButton>
-          <img src={this.props.currentImage} alt={this.props.tags} />
-        </s.Modal>
-      </s.Overlay>
-    );
-  }
+  return (
+    <s.Overlay onClick={handleBackdropClick}>
+      <s.Modal>
+        <s.CloseButton type="button" onClick={closeModal}>
+          <IconClose width="30" heigth="30" />
+          <s.ButtonLabel>Close</s.ButtonLabel>
+        </s.CloseButton>
+        <img src={currentImage} alt={tags} />
+      </s.Modal>
+    </s.Overlay>
+  );
 }
 
 Modal.propTypes = {
